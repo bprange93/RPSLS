eb5394103406f9f07411f2c40baa5a1a041ce5f0
Brandon Prange <bprange93@gmail.com>
2020-09-21 13:11:48 -0500

    Decided to start working on the Human class and trying to get the ChooseGesture method to work so the user to be able to pick a gesture. Feel as if I have got the ball rolling in the right direction. Planning to keep working on this to get it to work. Came to a stand still point. Looking into possible solutions will seek help from instructors soon.

--- RPSLS/RPSLS/Human.cs
+++ RPSLS/RPSLS/Human.cs
@@ -15,13 +15,21 @@ namespace RPSLS
         //constructor
         public Human() 
         {
-            
+           
         }
 
         //member methods
         public override void ChooseGesture()
         {
             //have user put their selection in for which they will choose
+            Console.WriteLine("Which gesture would you like to use?");
+            string[] gestures = { "rock", "paper", "scissors", "lizard", "spock" };
+            string response = Console.ReadLine().ToLower();
+            for (string rock)
+            {
+                if()
+            }
+
         }
     }
 }
